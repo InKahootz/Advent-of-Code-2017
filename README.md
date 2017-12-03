92de1258a60c555a401cbda05cb6d36b44d46c36
Hank McCord <hymccord@gmail.com>
2017-12-03 10:01:21 -0500

    Add readme, project runner, and day 1 solution

---
+++ README.md
@@ -0,0 +1,6 @@
+Advent of Code 2017
+
+Todo:
+* Make runner generic
+* Add unit tests
+* Convert all to dotnet core (?)
---
+++ Runner/Runner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Advent_of_Code.Runner
+{
+    public class Runner
+    {
+        static void Main(string[] args)
+        {
+            var day = new InverseCaptcha();
+            Console.WriteLine(day.Run());
+        }
+    }
+}
